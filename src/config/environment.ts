52e03f10bb9741a6a2106078db01b0d56a09ed5a
alexlop29 <alexander.lopez@owasp.org>
2024-01-16 09:15:54 -0500

    Configure Docker and Redis

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -10,3 +10,4 @@ export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const AWS_SESSION_TOKEN = process.env.AWS_SESSION_TOKEN;
 export const LAUNCH_DARKLY_SDK_KEY = process.env.LAUNCH_DARKLY_SDK_KEY ?? "";
+export const REDIS_URL = process.env.REDIS_URL ?? "";
