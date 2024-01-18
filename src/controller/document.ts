017549911bd10f43f55bcebe5ae2ac998d29e21c
alexlop29 <alexander.lopez@owasp.org>
2024-01-18 14:04:13 -0500

    Remediate DI Code Smell

--- src/controller/document.ts
+++ src/controller/document.ts
@@ -1,15 +1,11 @@
 import { s3 } from "../config/index.js";
 
 class Document {
-  private s3; // Declare a private property for the S3 instance
 
   constructor(
-    s3: any, 
     private readonly user: string,
     private readonly file: Express.Multer.File,
-  ) {
-    this.s3 = s3;
-  }
+  ) {}
 
   async save() {
     try {
@@ -18,7 +14,7 @@ class Document {
         Key: `${this.user}-${this.file.originalname}-${Math.random() * (100 - 50) + 50}`, // Arbitrary to allow multiple uplaods of the same doc for testing
         Body: this.file.buffer,
       };
-      await this.s3.upload(params).promise();
+      await s3.upload(params).promise();
       return { Status: 200, Message: "OK" };
     } catch (error) {
       throw { Status: 500, Message: error };
